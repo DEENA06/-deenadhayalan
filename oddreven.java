f8297ba3f07ecd1034161a0511c43ec8e2b9893c
DEENA06 <deenadhayalan.dd1997@gmail.com>
2017-06-07 10:09:21 +0530

    Add files via upload

---
+++ oddreven.java
@@ -0,0 +1,17 @@
+package deen;
+
+public class oddreven {
+
+	public static void main(String[] args) {
+		int a=3;
+		if(a%2==0)
+		{
+			System.out.println("even num");
+		}
+		else{System.out.println("odd num");
+	
+		}
+
+	}
+
+}
