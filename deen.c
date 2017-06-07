84d6125b63dc3a2e19ce0bce8afc1772adf95b65
DEENA06 <deenadhayalan.dd1997@gmail.com>
2017-06-07 09:40:27 +0530

    Add files via upload

---
+++ deen.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+int a;
+printf("enter the value");
+scanf("%d",&a);
+if(a>0)
+{
+printf("the number is positive");
+}
+else if(a<0)
+{
+printf("the number is negative");
+}
+else
+{
+printf("the number is zero");
+}
+return 0;
+}
