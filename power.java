a2e4a74ac27b2a262e4d33b31ff3df005455343c
DEENA06 <deenadhayalan.dd1997@gmail.com>
2017-06-14 09:46:56 +0530

    Add files via upload

---
+++ power.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+ class power {
+
+	public static void main(String[] args) {
+		System.out.println("enter the number");
+		double a, r;
+		Scanner s=new Scanner(System.in);
+		a=s.nextInt();
+		r=s.nextInt();
+		System.out.println(Math.pow(a,r));
+		
+
+	}
+
+}
