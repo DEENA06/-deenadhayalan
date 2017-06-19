c143836877ea7eff760ed335b6f28eecb9a5ef0d
DEENA06 <deenadhayalan.dd1997@gmail.com>
2017-06-19 15:08:00 +0530

    Add files via upload

---
+++ rearray.java
@@ -0,0 +1,26 @@
+    class Simple{
+    public static void main(String args[])
+     
+    {
+     
+            int array[] = {2,2,2,1,5,4,6,8};
+            int size = array.length;
+            int max=0;
+            System.out.println("Size before deletion: " + size);
+ 
+            for (int i=0;i<size;i++) 
+            {
+ 
+                for (int j=i+1;j<size;j++) 
+                {
+ 
+ 
+                    if (array[i]==array[j]) 
+                    {
+                      max=array[i];
+                    }
+                      }
+                     }
+System.out.println(max);
+}
+}
