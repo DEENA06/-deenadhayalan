e6f7e33303aeafcb52aae6e2c1747c6247a16670
DEENA06 <deenadhayalan.dd1997@gmail.com>
2017-07-11 11:44:55 +0530

    Add files via upload

---
+++ ddd.java
@@ -0,0 +1,15 @@
+package deena;
+
+public class ddd {
+	public static void main(String args[]) {
+		String s1="eMesreveR";
+		String s2="devomeRslewoV";
+		String strOrig;
+		String s3 = s1.replaceAll("[aeiouAEIOU]", "");
+		String s4 = s2.replaceAll("[aeiouAEIOU]", "");
+		System.out.println(s3);
+		System.out.println(s4);
+	}
+	}
+
+
