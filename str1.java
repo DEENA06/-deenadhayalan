62029c46a7060f70f05c5bbcc8e468f20a3e170f
DEENA06 <deenadhayalan.dd1997@gmail.com>
2017-07-11 17:27:22 +0530

    Add files via upload

--- str1.java
+++ str1.java
@@ -7,11 +7,7 @@ public class str1 {
 		Scanner in=new Scanner(System.in);
 		String str=in.nextLine();
 		String str1=new StringBuffer(str).reverse().toString();
-		/*System.out.println(b);
-		for(int i=n-1;i<n;i--) {
-			str1=b+b.charAt(i)+"-";
-		}
-		System.out.println(str1);*/
+		
 		int n=str1.length();
 		for(int i=0;i<n;i++) {
 			char str2=str1.charAt(i);
