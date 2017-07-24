4a54456870176faa38a3d060f3bb315a998252c6
DEENA06 <deenadhayalan.dd1997@gmail.com>
2017-07-24 15:02:41 +0530

    Add files via upload

--- str1.java
+++ str1.java
@@ -1,21 +1,27 @@
-package deena;
+package com.java.guvi;
 import java.util.Scanner;
 public class str1 {
 
 	public static void main(String[] args) {
-		System.out.println("enter the string");
-		Scanner in=new Scanner(System.in);
-		String str=in.nextLine();
-		String str1=new StringBuffer(str).reverse().toString();
 		
-		int n=str1.length();
-		for(int i=0;i<n;i++) {
-			char str2=str1.charAt(i);
-			System.out.print(str2+"-");
-			
-		}
-		
+			    Scanner in=new Scanner(System.in);
+			    System.out.println("ENTER STRING ARRAY LENGTH");
+			    int len=in.nextInt();
+			    String arr[]=new String[len];
+			    for(int i=0;i<len;i++) {
+			       arr[i]=in.next();
+			    }
+			    for(int j=0;j<len;j++)
+			    {
+			      if(arr[j].length()==1)
+			      {
+			         System.out.println(arr[j]);
+			        
+			       }
+			      }
+			    }
+	
 
 	}
 
-}
+
