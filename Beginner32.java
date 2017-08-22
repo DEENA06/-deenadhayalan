c5d11bf30a0e414908ca51c22f605afa3ce99b6e
DEENA06 <deenadhayalan.dd1997@gmail.com>
2017-08-22 16:00:45 +0530

    Add files via upload

---
+++ Beginner32.java
@@ -0,0 +1,21 @@
+package com.deen.wipro;
+import java.util.*;
+public class Beginner32 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String s = sc.nextLine();
+ 
+		int count = 1;
+ 
+		for (int i = 0; i < s.length() - 1; i++)
+		{
+			if ((s.charAt(i) == ' ') && (s.charAt(i + 1) != ' '))
+			{
+				count++;
+ 
+			}
+		}
+ 		System.out.println("Number of words in a string = " + count);
+	}
+}
